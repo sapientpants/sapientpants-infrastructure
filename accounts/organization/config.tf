a2c18866e0e4d15e7ea6f541afa21e53c065cfde
sapientpants <sapientpants@users.noreply.github.com>
2023-01-02 22:24:18 -0500

    Use per-request billing for dynamodb lock table

--- accounts/organization/config.tf
+++ accounts/organization/config.tf
@@ -11,8 +11,7 @@ terraform {
 resource "aws_dynamodb_table" "sapientpants_terraform" {
   name           = "sapientpants-terraform"
   hash_key       = "LockID"
-  read_capacity  = 20
-  write_capacity = 20
+  billing_mode   = "PAY_PER_REQUEST"
 
   server_side_encryption {
     enabled = true
